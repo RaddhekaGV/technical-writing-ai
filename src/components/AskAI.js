1b419df8565f964eb3383ef6e5d446df6a09f6ce
Raddheka GV <raddheka.cybage@gmail.com>
2026-07-09 17:38:50 +0530

    Fix API endpoint

--- src/components/AskAI.js
+++ src/components/AskAI.js
@@ -15,7 +15,7 @@ export default function AskAI() {
     setQuestion("");
 
     try {
-      const res = await fetch(/api/ask, {
+      const res = await fetch("/api/ask", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ question: userMsg.text }),
